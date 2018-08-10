65dd951496c06bb71afbf281229c681442e284e7
Jorge L <jorge.alfmur@gmail.com>
2018-08-10 00:41:55 -0500

    estilos a componente playlist

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Media from './media';
-import Responsive from '../../images/covers/responsive.jpg';
+import './playlist.css';
 
 class Playlist extends Component {
   render() {
     const playlist = this.props.data.categories[0].playlist;
     return (
-      <div>
+      <div className="Playlist">
         {
           playlist.map((item) => (
             <Media
