3bcc2ea5315414309597bd747533eab11a366a99
Jorge L <jorge.alfmur@gmail.com>
2018-08-10 21:48:44 -0500

    functional component

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Media from './media';
 import './playlist.css';
 
-class Playlist extends Component {
-  render() {
-    const playlist = this.props.data.categories[0].playlist;
-    return (
-      <div className="Playlist">
-        {
-          playlist.map((item) => (
-            <Media
-              key={item.id}
-              {...item}
-            />
-          )
+function Playlist(props) {
+  const playlist = props.data.categories[0].playlist;
+  return (
+    <div className="Playlist">
+      {
+        playlist.map((item) => (
+          <Media
+            key={item.id}
+            {...item}
+          />
         )
-        }
-      </div>
-    );
-  }
+      )
+      }
+    </div>
+  );
 }
 
 Playlist.propTypes = {
