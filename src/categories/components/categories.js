502afe0f5f1916326213653a44987789998bbe0f
Jorge L <jorge.alfmur@gmail.com>
2018-08-19 22:25:59 -0500

    UI formulario de busqueda

--- src/categories/components/categories.js
+++ src/categories/components/categories.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Category from './category';
+import SearchContainer from '../../widgets/containers/search';
 import './categories.css';
 
 function Categories(props) {
   return (
     <div className="Categories">
+      <SearchContainer/>
       {
         props.categories.map((category) => (
           <Category key={category.id} {...category} handleClick={props.handleOpenModal}/>
---
+++ src/widgets/components/search.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './search.css';
+
+const Search = ()  => (
+  <form className="Search">
+    <input
+      className="Search-input"
+      type="text"
+      placeholder="Busca tu video favorito"
+    />
+  </form>
+);
+
+export default Search;
