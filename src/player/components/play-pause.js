8b73c81c3bc3d2a301b96107bf1923bb0181dd30
Jorge L <jorge.alfmur@gmail.com>
2018-08-20 21:31:34 -0500

    boton play-pausa sin logica de momento

---
+++ src/player/components/play-pause.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Play from '../../icons/components/play';
+import Pause from '../../icons/components/pause';
+import './play-pause.css';
+
+const PlayPause = (props) => (
+  <div className="PlayPause">
+    {
+      props.pause ?
+      <button onClick={props.handleClick}>
+        <Pause color="white" size={25}/>
+      </button>
+      :
+      <button onClick={props.handleClick}>
+        <Play color="white" size={25}/>
+      </button>
+    }
+  </div>
+);
+
+PlayPause.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
+
+export default PlayPause;
--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Title from '../components/title';
 import Video from '../components/video';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component {
+  state = {
+    pause: false,
+  };
+
+  togglePlay = () => {
+    this.setState(prevState => ({
+      pause: !prevState.pause,
+    }));
+  };
+
   render() {
     return (
       <VideoPlayerLayout>
         <Title title="Hola Crrano"/>
+        <PlayPause pause={this.state.pause} handleClick={this.togglePlay}/>
         <Video
           autoplay={false}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
-      />
+        />
       </VideoPlayerLayout>
     );
   }
 }
+
+VideoPlayer.propTypes = {
+  pause: PropTypes.bool.isRequired,
+};
 
 export default VideoPlayer;
