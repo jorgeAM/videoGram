7c6fcce6b33324a37cefc0dab85260c3af4c6d40
Jorge L <jorge.alfmur@gmail.com>
2018-11-05 01:10:00 -0500

    implementamos react-router v1

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import Home from '../pages/containers/home';
+import Videos from '../pages/containers/videos';
+import Header from '../pages/components/header';
+import Home from '../pages/components/home';
 import data from '../api.json';
 /*IMPORTAMOS STORE*/
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from '../reducers/data';
+/*REACT-ROUTER*/
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const initialState = {
   data: {
@@ -21,8 +25,16 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Home/>
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <Fragment>
+        <Header/>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/videos' component={Videos}/>
+        <Route exact path='/contacto' component={Home}/>
+        <Route exact path='/perfil' component={Home}/>
+      </Fragment>
+    </Provider>
+  </Router>,
   document.getElementById('app')
 );
--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -6,7 +6,6 @@ import './related.css';
 function Related(props) {
   return (
     <div className="Related">
-      <img src={logo} width={250}/>
     </div>
   );
 }
