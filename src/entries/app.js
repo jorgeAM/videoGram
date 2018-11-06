3ccebad7bb487293c0fde670b35818f293c06178
Jorge L <jorge.alfmur@gmail.com>
2018-11-05 19:51:37 -0500

    componente para error 404

--- src/entries/app.js
+++ src/entries/app.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import Videos from '../pages/containers/videos';
 import Header from '../pages/components/header';
 import Home from '../pages/components/home';
+import NotFound from '../pages/components/not-found';
 import data from '../api.json';
 /*IMPORTAMOS STORE*/
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from '../reducers/data';
 /*REACT-ROUTER*/
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const initialState = {
   data: {
@@ -29,10 +30,12 @@ ReactDOM.render(
     <Provider store={store}>
       <Fragment>
         <Header/>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/videos' component={Videos}/>
-        <Route exact path='/contacto' component={Home}/>
-        <Route exact path='/perfil' component={Home}/>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/videos' component={Videos}/>
+          <Route exact path='/contacto' component={Home}/>  
+          <Route component={NotFound}/>
+        </Switch>
       </Fragment>
     </Provider>
   </Router>,
